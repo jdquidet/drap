b64c0b1da67f697e0642b402bdc9582ef38804d5
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2026-04-01 04:11:27 +0800

    refactor: add breakpoints to student card for smaller screens

--- src/lib/users/student.svelte
+++ src/lib/users/student.svelte
@@ -25,18 +25,18 @@
   href="mailto:{email}"
   class="grid w-full grid-cols-[auto_1fr] items-center gap-2 rounded-md bg-muted p-2 transition-colors duration-150 hover:bg-muted/80"
 >
-  <Avatar.Root class="size-20">
+  <Avatar.Root class="size-14 md:size-20">
     <Avatar.Image src={avatarUrl} alt="{givenName} {familyName}" />
     <Avatar.Fallback>{givenName[0]}{familyName[0]}</Avatar.Fallback>
   </Avatar.Root>
-  <span class="flex flex-col">
+  <span class="flex flex-col overflow-hidden">
     {#if givenName.length > 0 && familyName.length > 0}
       <strong><span class="uppercase">{familyName}</span>, {givenName}</strong>
     {/if}
     {#if studentNumber !== null}
       <span class="text-sm opacity-50">{studentNumber}</span>
     {/if}
-    <span class="text-xs opacity-50">{email}</span>
+    <span class="truncate text-xs opacity-50">{email}</span>
     <div class="space-x-1">
       {#if labId === null}
         <PreferredLab {labs} />
